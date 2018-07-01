e6f5657f079d0018b09546d22295c82c7ee13386
Matteo Hertel <info@matteohertel.com>
2018-07-01 20:33:38 +0100

    adding test enpoints

--- youtube-bridge/main.go
+++ youtube-bridge/main.go
@@ -7,12 +7,15 @@ import (
 
 func main() {
 	port := GetEnv("PORT", "3460")
-	fmt.Println(port)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.HandleFunc("/", index)
+	http.HandleFunc("/wadup", wadup)
 	http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
 }
 
+func wadup(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(res, "Wad Up??!")
+}
 func index(res http.ResponseWriter, req *http.Request) {
-	fmt.Println("wad up")
+	fmt.Fprintln(res, "Wad Up?!")
 }

7a89ca1150d4467fb703ec7e8ccaded42d73af09
RobStrover <robert.strover@fuelintegrated.com>
2018-08-18 23:21:04 +0100

    [MB] Move link-youtube and link-spotify buttons into components

---
+++ music-bridge/components/InternalLinkButton.vue
@@ -0,0 +1,13 @@
+<template>
+
+    <nuxt-link v-if="buttonDependency" :to="linkTo" class="btn btn-primary btn-lg">
+        {{ buttonMessage }}
+    </nuxt-link>
+
+</template>
+
+<script>
+    export default {
+        props: ["linkTo", "buttonDependency", "buttonMessage"],
+    };
+</script>
---
+++ music-bridge/components/LoginButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <b-button
+            :disabled="!url"
+            :href="url"
+            v-if="buttonDependency"
+            size="lg"
+            variant="primary"
+    >
+        <span v-if="url">{{ buttonMessage }}</span>
+        <span v-else>{{ waitMessage }}</span>
+    </b-button>
+</template>
+
+<script>
+    export default {
+        props: ["url", "waitMessage", "buttonMessage", "buttonDependency"],
+    };
+</script>
